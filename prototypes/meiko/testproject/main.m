39de525f95bc0c44d21fe155576ea8b25da4fa4d
meikle_li@hotmail.com <meikle_li@hotmail.com@304996ac-fee6-11de-9d03-c52165109085>
2010-02-13 01:33:08 +0000

    added meikos test project

---
+++ prototypes/meiko/testproject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testproject
+//
+//  Created by Meiko on 10-02-12.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
