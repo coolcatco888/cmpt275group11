4441b9aab6572d6d83597b43bc81db4adc0d4044
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-02-15 21:42:30 +0000

    Imported local copy

---
+++ trunk/documents/cmpt275group11/prototypes/cley/QuizMonkey/Classes/RootViewController.h
@@ -0,0 +1,12 @@
+//
+//  RootViewController.h
+//  QuizMonkey
+//
+//  Created by Cley on 12/02/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
