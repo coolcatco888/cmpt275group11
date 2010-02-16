c92b8dcff177d0af73373ce6479478331159d042
DrakoDoom@hotmail.com <DrakoDoom@hotmail.com@304996ac-fee6-11de-9d03-c52165109085>
2010-02-16 03:38:06 +0000

    Created Main Menu interface not linked yet but does contain auto screen rotation functionality

---
+++ prototypes/ariel/QuizMonkey/Classes/QuizMonkeyViewController.h
@@ -0,0 +1,16 @@
+//
+//  QuizMonkeyViewController.h
+//  QuizMonkey
+//
+//  Created by Ariel Lorenzo-Luaces on 2/15/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizMonkeyViewController : UIViewController {
+
+}
+
+@end
+
