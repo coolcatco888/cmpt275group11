c34df5ad06e930d99610f9f6f641607a2ececaa6
qya7@sfu.ca <qya7@sfu.ca@304996ac-fee6-11de-9d03-c52165109085>
2010-02-16 17:06:04 +0000

    pick up questions with ui

--- trunk/prototypes/tony/QuizMonkey/Classes/QuizMonkeyViewController.h
+++ trunk/prototypes/tony/QuizMonkey/Classes/QuizMonkeyViewController.h
@@ -15,9 +15,18 @@
 @interface QuizMonkeyViewController : UIViewController {
 
 	IBOutlet UILabel* question_label;
+	IBOutlet UILabel* result;
 	IBOutlet UIView* main;
 	
-	PickOutWordsQuestion* q1;
+	IBOutlet UIButton* ans1;
+	IBOutlet UIButton* ans2;
+	IBOutlet UIButton* ans3;
+	IBOutlet UIButton* ans4;
+	NSMutableArray* question_array;
+	int num;
+	int total;
+	Question* current_question;
+	//Question* q1;
 	
 	
 	
