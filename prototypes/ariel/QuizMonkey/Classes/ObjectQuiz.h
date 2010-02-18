c3715041b63deb05698c1a22d7c72d1f119270e7
DrakoDoom@hotmail.com <DrakoDoom@hotmail.com@304996ac-fee6-11de-9d03-c52165109085>
2010-02-18 01:46:06 +0000

    Button movement and touch movement added. Working on button loading

---
+++ prototypes/ariel/QuizMonkey/Classes/ObjectQuiz.h
@@ -0,0 +1,16 @@
+//
+//  ObjectQuiz.h
+//  QuizMonkey
+//
+//  Created by Ariel Lorenzo-Luaces on 2/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ObjectQuiz : NSObject {
+
+}
+
+@end
