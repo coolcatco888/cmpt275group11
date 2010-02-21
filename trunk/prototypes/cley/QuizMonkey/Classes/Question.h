22d4ae9c4ac8cf88be5f04c7ad1666add4772c12
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-02-21 00:52:31 +0000

    Added new methods to Question for ease of use with QuestionParser

--- trunk/prototypes/cley/QuizMonkey/Classes/Question.h
+++ trunk/prototypes/cley/QuizMonkey/Classes/Question.h
@@ -8,24 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
-
+//TODO: Maybe we can just have one question object?
 @interface Question : NSObject {
-	NSString* type;
-	NSString* image;
-	NSMutableSet* answers;
-	int time;
+	NSString* type;//Specifies the type of question, FillinTheBlank or PickOutWords Question
+	NSString* image;//URL of the image that is to be displayed
+	NSMutableSet* answers;//The set containing correct answers, some questions will require the student to pick out the verbs
+	NSMutableSet* choices;//The set containing the options available for the student to choose
+	int time;//How much time is allotted for this type of question
 
 }
 
 @property (assign) NSString* type;
 @property (assign) NSString* image;
-@property (assign) int time;
 @property (assign) NSMutableSet* answers;
+@property (assign) NSMutableSet* choices;
+@property (assign) int time;
+
 
 
 -(NSArray*) validateAnswers: (NSSet*) answers;
 -(void) setType: (NSString*) newType;
 -(void) setImage: (NSString*) newImage;
 -(void) setAnswers: (NSMutableSet*) answers;
+-(void) setChoices: (NSMutableSet*) choices;
+-(void) addAnswer: (NSString*) answer;
+-(void) addChoice: (NSString*) choice;
 
 @end
