8c1adc5279e89d6a7049ac967aa4c4fd4db845d1
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-02-24 23:48:41 +0000

    Changed to make answers have weighted points

--- trunk/prototypes/cley/QuizMonkey/Classes/Question.h
+++ trunk/prototypes/cley/QuizMonkey/Classes/Question.h
@@ -13,8 +13,9 @@
 	NSString* type;//Specifies the type of question, FillinTheBlank or PickOutWords Question
 	NSString* image;//URL of the image that is to be displayed
 	NSString* sentence;//This is for FillInTheBlank Questions
-	NSMutableSet* answers;//The set containing correct answers, some questions will require the student to pick out the verbs
-	NSMutableSet* choices;//The set containing the options available for the student to choose
+	NSMutableArray* answers;//The set containing correct answers, some questions will require the student to pick out the verbs
+	NSMutableArray* choices;//The set containing the options available for the student to choose
+	NSMutableArray* points;//The set containing the points associated with each answer, the number should match the number of answers
 	int time;//How much time is allotted for this type of question
 
 }
@@ -22,8 +23,9 @@
 @property (assign) NSString* type;
 @property (assign) NSString* image;
 @property (assign) NSString* sentence;
-@property (assign) NSMutableSet* answers;
-@property (assign) NSMutableSet* choices;
+@property (assign) NSMutableArray* answers;
+@property (assign) NSMutableArray* choices;
+@property (assign) NSMutableArray* points;
 @property (assign) int time;
 
 
@@ -32,9 +34,11 @@
 -(void) setType: (NSString*) newType;
 -(void) setImage: (NSString*) newImage;
 -(void) setSentence: (NSString*) newSentence;
--(void) setAnswers: (NSMutableSet*) answers;
--(void) setChoices: (NSMutableSet*) choices;
+-(void) setAnswers: (NSMutableArray*) answers;
+-(void) setChoices: (NSMutableArray*) choices;
+-(void) setPoints: (NSMutableArray*) points;
 -(void) addAnswer: (NSString*) answer;
 -(void) addChoice: (NSString*) choice;
+-(void) addPoint: (NSInteger*) point;
 
 @end
