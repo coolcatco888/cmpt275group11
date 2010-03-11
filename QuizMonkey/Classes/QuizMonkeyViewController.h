e95bb02d7a8a3899d7116553782c0bec451ae966
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-03-11 00:01:08 +0000

    Branched ariel's code to a main project

---
+++ QuizMonkey/Classes/QuizMonkeyViewController.h
@@ -0,0 +1,66 @@
+//
+//  QuizMonkeyViewController.h
+//  QuizMonkey
+//
+//  Created by Ariel Lorenzo-Luaces on 2/15/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizMonkeyViewController : UIViewController {
+	//NSObject ObjectQuestion *Quest;
+	//ClassQuestionParser *Quest;
+	//ObjectQuestion *Questions;
+	IBOutlet UIView *vew_MainMenu;
+	IBOutlet UIView *vew_Question;
+	IBOutlet UIView *vew_HighScores;
+	IBOutlet UIButton *btn_Play;
+	IBOutlet UIButton *btn_Quit;
+	IBOutlet UIButton *btn_New;
+	IBOutlet UIImageView *img_Monkey;
+	IBOutlet UILabel *lbl_Question;
+	IBOutlet UILabel *lbl_New;
+	UIAlertView *alr_Alert;
+	NSArray *ThisArray;
+	IBOutlet UIScrollView *scr_HighScores;
+	IBOutlet UITableViewCell *cel_Score1;
+	IBOutlet UITableView *tbl_HighScores;
+	CGPoint TapLocation;
+	IBOutlet UILabel *lbl_Y;
+	NSXMLParser *QuestionParser;
+	
+
+	//Question Window
+	IBOutlet UILabel *lbl_Type;
+	IBOutlet UILabel *lbl_pic_Name;
+	IBOutlet UILabel *lbl_Sentence;
+	IBOutlet UILabel *lbl_Choice1_Word;
+	IBOutlet UILabel *lbl_Choice2_Word;
+	IBOutlet UILabel *lbl_Choice3_Word;
+	IBOutlet UILabel *lbl_Choice4_Word;
+	IBOutlet UILabel *lbl_Choice1_Points;
+	IBOutlet UILabel *lbl_Choice2_Points;
+	IBOutlet UILabel *lbl_Choice3_Points;
+	IBOutlet UILabel *lbl_Choice4_Points;
+	IBOutlet UILabel *lbl_Time;
+
+}
+
+-(IBAction)ShowQuestionView:(id)sender;
+-(IBAction)ExitQuestionView:(id)sender;
+-(IBAction)ShowHighScoresView:(id)sender;
+-(IBAction)ExitHighScoresView:(id)sender;
+
+-(IBAction)SetCell:(id)sender;
+-(IBAction)LoadObjectQuestion:(id)sender;
+
+-(IBAction)ShowAlert:(id)sender;
+-(IBAction)NewButton:(id)sender;
+-(IBAction)Change_Button:(id)sender;
+-(IBAction)Change_Picture:(id)sender;
+-(IBAction)LoadArray:(id)sender;
+-(IBAction)SetQuestion:(id)sender;
+
+@end
+
