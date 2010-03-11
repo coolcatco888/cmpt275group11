5cbfd0fea16e6a2e90d7338626cb26eabc3a641a
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-03-11 00:01:08 +0000

    Branched ariel's code to a main project

---
+++ trunk/QuizMonkey/Classes/ObjectQuestion.h
@@ -0,0 +1,47 @@
+//
+//  ObjectQuestion.h
+//  QuizMonkey
+//
+//  Created by Ariel Lorenzo-Luaces on 2/17/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ObjectQuestion : NSObject {
+	NSString *Type;
+	NSString *pic_Name;
+	NSString *Sentence;
+	NSMutableArray *Choices_Words;
+	NSMutableArray *Choices_Points;
+	NSInteger Time;
+}
+
+@property (assign) NSString* Type;
+@property (assign) NSString* pic_Name;
+@property (assign) NSString* Sentence;
+@property (assign) NSMutableArray* Choices_Words;
+@property (assign) NSMutableArray* Choices_Points;
+@property (assign) NSInteger Time;
+
+@end
+
+@implementation ObjectQuestion
+
+@synthesize Type;
+@synthesize pic_Name;
+@synthesize Sentence;
+@synthesize Choices_Words;
+@synthesize Choices_Points;
+@synthesize Time;
+
+-(void) addChoices_Words: (NSString*) newChoice {
+	[Choices_Words addObject:newChoice];
+}
+
+-(void) addChoices_Points: (NSInteger) newPoint {
+	[Choices_Points addObject:[NSNumber numberWithInteger:newPoint]];
+}
+
+@end
