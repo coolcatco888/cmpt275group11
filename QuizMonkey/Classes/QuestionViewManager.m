04145482c96b6656d6f7c754d74f12e89255d198
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-03-14 08:46:35 +0000

    Everything is working, still need to implement online score submission and pick out the ___ something questions

--- QuizMonkey/Classes/QuestionViewManager.m
+++ QuizMonkey/Classes/QuestionViewManager.m
@@ -27,10 +27,12 @@
 
 -(QuestionViewManager*) initQuestionViewManager:(UIView*) mainView 
 											   :(UIView*) questionView 
+											   :(UIView*) finalScoreView 
 											   :(UILabel*) sentenceLabel 
 											   :(UILabel*) sentenceLabelBottom 
 											   :(UILabel*) questionType
-											   :(UIImageView*) image
+											   :(UILabel*) finalScore
+											   :(UIImageView*) image 
 											   :(UIImageView*) monkey
 											   :(UIProgressView*) progress
 											   :(NSArray*) questionButtons {
@@ -39,9 +41,11 @@
 	//Setup from parameters
 	mainMenuScreen = mainView;
 	questionScreen = questionView;
+	finalScoreScreen = finalScoreView;
 	questionSentenceLabel = sentenceLabel;
 	questionSentenceBottomLabel = sentenceLabelBottom;
 	questionTypeLabel = questionType;
+	finalScoreLabel = finalScore;
 	questionImage = image;
 	questionChoiceButtons = questionButtons; 
 	monkeyImage = monkey;
@@ -57,7 +61,7 @@
 	selectedChoices = [NSMutableSet setWithCapacity:4];
 	totalPointsAcquired = 0;
 	
-	//Randomly select 10 questions
+	//Randomly select 10 questions and Display the questionScreen
 	questionList = [self select10Questions:questionLibrary];
 	currentQuestionIndex = 0;
 	[self loadQuestionFromIndex:currentQuestionIndex];
@@ -83,9 +87,9 @@
 	for(int i = 0; i < [questionChoiceButtons count]; i++) {
 		//Match the selected choice with the available choices
 		if([choice isEqualToString:((UIButton*)[questionChoiceButtons objectAtIndex:i]).titleLabel.text]) {
+			
 			//If this choice is already selected then remove it
 			NSNumber* currentSelection = [NSNumber numberWithInt:i];
-			
 			if([selectedChoices containsObject:currentSelection]) {
 				[selectedChoices removeObject:currentSelection];
 				[buttonPressed setSelected:FALSE];
@@ -108,6 +112,7 @@
 		[questions  exchangeObjectAtIndex:i withObjectAtIndex:n];
 	}
 	
+	//If the xml file contains less than 10 questions then iterate only through < 10 questions
 	NSUInteger size = [questions count] < 10? [questions count] : 10;
 	totalPoints = 0;
 	totalTime = 0;
@@ -133,23 +138,29 @@
 }
 
 -(IBAction)nextQuestion:(id)sender {
+	//Pause timer
 	[timer invalidate];
 	int points = 0;
 	[monkeyImage setHidden:FALSE];
 	NSArray* pointList = currentQuestion.points;
 	
+	//Add Up total points and add the score to the totalPointsAquired (by student)
 	for(id index in selectedChoices) {
 		NSNumber* pointIndex = (NSNumber*)index;
 		NSNumber* pointValue = (NSNumber*) [pointList objectAtIndex:[pointIndex intValue]];
 		points += [pointValue intValue];
 	}
-	totalPointsAcquired = points;
+	totalPointsAcquired += points;
 	
+	//Display an appropriate message given the reslults
 	NSString* title;
 	NSMutableString* message = [NSMutableString stringWithCapacity:100];
+	
+	//Set message if the student got nothing
 	if(points == 0) {
 		title = @"Sorry...";
 		[message appendString:@"Your answer is completely WRONG!"];
+	//Set message if the student got some right
 	} else if (points < totalPointsForCurrentQuestion) {
 		title = @"Not Bad...";
 		[message appendString:@"You got "];
@@ -157,6 +168,7 @@
 		[message appendString:@"/"];
 		[message appendString:[[NSNumber numberWithInt:totalPointsForCurrentQuestion] stringValue]];
 		[message appendString:@"!"];
+	//Set Message if the student got all of them right
 	} else {
 		title = @"Perfect!";
 		[message appendString:@"Your answer is correct!"];
@@ -174,10 +186,13 @@
 }
 
 -(void) loadQuestionFromIndex: (NSUInteger) index {
+	//Delete current question in memory and add a new question
 	[currentQuestion release];
 	currentQuestion = (Question*) [questionList objectAtIndex:index];
 	NSLog(@"New Question Set at Index:");
 	NSLog([[NSNumber numberWithInt:index] stringValue]);
+	
+	//Here is where the screen objects are set from the question object
 	if([currentQuestion.type isEqualToString:@"Fill in the blank"]
 		|| [currentQuestion.type isEqualToString:@"Pick Out the Words"]) {
 		NSLog(@"Did all dat");
@@ -187,12 +202,13 @@
 		[questionSentenceBottomLabel setHidden:TRUE];
 		[questionImage setHidden:TRUE];
 		
+		//Set all of the text for the choice buttons
 		for(int i = 0; i < [questionChoiceButtons count]; i++) {
 			[((UIButton*)[questionChoiceButtons objectAtIndex:i]) setTitle:[currentQuestion.choices objectAtIndex:i] forState:0];
 		}
 		
+		//Calculate Question Totals
 		maxNumberOfChoiceSelections = [self getMaxNumberOfChoiceSelections:currentQuestion.points];
-		
 		totalPointsForCurrentQuestion = [self calculateTotalScore: currentQuestion.points];
 		totalPoints += totalPointsForCurrentQuestion;
 		NSLog(@"Max Points allowable is:");
@@ -220,11 +236,27 @@
 			if(currentTimeLeft == 0) {
 				//Exit Quiz Session
 				[self quitGame];
+			//Load Next Question if the index is less than the number of questions - 1 (since it is zero indexed)
 			} else if(currentQuestionIndex < [questionList count] - 1) {
 				NSLog(@"Next Question");
 				currentQuestionIndex++;
 				[self loadQuestionFromIndex:currentQuestionIndex];
 				timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+			//If we have reached the end of the questions
+			} else if (currentQuestionIndex == [questionList count] - 1) {
+				//Quit game and show final score screen
+				[self quitGame];
+				[mainMenuScreen addSubview:finalScoreScreen];
+				
+				//Set final score on finalScoreScreen
+				NSMutableString* score = [NSMutableString stringWithCapacity:100];
+				[score appendString:@"You got "];
+				[score appendString:[[NSNumber numberWithInt:totalPointsAcquired] stringValue]];
+				[score appendString:@" out of "];
+				[score appendString:[[NSNumber numberWithInt:totalPoints] stringValue]];
+				[score appendString:@" banana points!"];
+				[finalScoreLabel setText:score];
+				
 			}
 			break;
 	}
@@ -232,14 +264,18 @@
 
 -(void) resetAllButtons {
 	for(int i = 0; i < [questionChoiceButtons count]; i++) {
+		//Set state back to normal
 		[((UIButton*)[questionChoiceButtons objectAtIndex:i]) setSelected:FALSE];
 		[monkeyImage setHidden:TRUE];
 	}
 	[questionChoiceButtons retain];
 }
 -(void) updateTimer {
+	//Decrement by one second at a time
 	currentTimeLeft--;
+	//Update timer bar
 	[timerProgress setProgress: ((float) currentTimeLeft / (float) totalTime)];
+	//If the student ran out of time, exit to the main menu
 	if (currentTimeLeft == 0) {
 		[timer invalidate];
 		[monkeyImage setHidden:FALSE];
