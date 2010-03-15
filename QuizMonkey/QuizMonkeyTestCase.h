33f135ace0a6440c1ce607b936740f4861d172c0
qya7@sfu.ca <qya7@sfu.ca@304996ac-fee6-11de-9d03-c52165109085>
2010-03-15 06:43:36 +0000

    added comments for QuizMonkeyTestCase

--- QuizMonkey/QuizMonkeyTestCase.h
+++ QuizMonkey/QuizMonkeyTestCase.h
@@ -2,6 +2,11 @@
 //  QuizMonkeyTestCase.h
 //  QuizMonkey
 //
+//	This is head file of XML parser testing. At first, parser decode a String,
+//	which contains XML documents, and then it produce a question object. After that,
+//	this test will check every attribute of the question, and it will show up message(s)
+//	if test is failed.
+//
 //  Created by Tony Yang on 3/14/10.
 //
 //  Worked on by Cley, Ariel, Tony, Meiko, Daniel
@@ -15,12 +20,11 @@
 //
 
 #import <SenTestingKit/SenTestingKit.h>
-
 #import "QuestionParser.h"
-#import "Question.h"
+
 
 @interface QuizMonkeyTestCase : SenTestCase {
-	QuestionParser* parser;
+	
 }
-- (void)testTestFramework;
+- (void)testTestFramework;//testing method
 @end
