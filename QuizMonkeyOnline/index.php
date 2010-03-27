e9fa2fae1a1d3de6b42e93c06001d0e962535dca
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-03-27 00:34:32 +0000

    Finished teacher login page

--- QuizMonkeyOnline/index.php
+++ QuizMonkeyOnline/index.php
@@ -5,12 +5,34 @@ and open the template in the editor.
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <?php
+        include_once('includes/head.inc');
+        ?>
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id='content'>
+            <img src="images/banner.png" alt="Quiz Monkey Student Progress Monitoring System"/>
+            <h1>Student Progress Monitoring System</h1>
+            <div id='loginbox' class='display'>
+                <h2>Teacher Login</h2>
+                <table id="logintable">
+                    <tbody>
+                        <tr>
+                            <td>Username:</td>
+                            <td><input type="text" name="user" value="" /></td>
+                        </tr>
+                        <tr>
+                            <td>Password:</td>
+                            <td><input type="password" name="pass" value="" /></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><a href="#">Reset</a> <a href="#">Login</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </body>
 </html>
