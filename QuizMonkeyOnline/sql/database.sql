2d09e355d806660b7d59469c4d14ef28682c5ad7
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-03-28 21:02:56 +0000

    submit.php able to store values in database

--- QuizMonkeyOnline/sql/database.sql
+++ QuizMonkeyOnline/sql/database.sql
@@ -1,17 +1,39 @@
 -- phpMyAdmin SQL Dump
--- version 3.1.2deb1ubuntu0.2
+-- version 3.2.2.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 28, 2010 at 12:15 PM
--- Server version: 5.0.75
--- PHP Version: 5.2.6-3ubuntu4.2
+-- Generation Time: Mar 28, 2010 at 02:01 PM
+-- Server version: 5.1.37
+-- PHP Version: 5.2.10-2ubuntu6.4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Database: `quizmonkey`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scores`
+--
+
+DROP TABLE IF EXISTS `scores`;
+CREATE TABLE IF NOT EXISTS `scores` (
+  `userid` varchar(50) NOT NULL,
+  `scoreid` int(255) NOT NULL,
+  `timeleft` int(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `points` int(255) NOT NULL,
+  `maxpoints` int(255) NOT NULL,
+  PRIMARY KEY (`userid`,`scoreid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `scores`
+--
+
 
 -- --------------------------------------------------------
 
@@ -27,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(20) NOT NULL,
   `email` varchar(100) NOT NULL,
   `type` int(32) NOT NULL,
-  PRIMARY KEY  (`userid`)
+  PRIMARY KEY (`userid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Stores accounts of students and teachers';
 
 --
