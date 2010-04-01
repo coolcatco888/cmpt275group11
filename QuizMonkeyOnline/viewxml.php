5528885b2df77d6e0d3e073b0b0569952c007a3e
coolcatco888 <coolcatco888@304996ac-fee6-11de-9d03-c52165109085>
2010-04-01 01:27:36 +0000

    Added error checking and login for score viewing

--- QuizMonkeyOnline/viewxml.php
+++ QuizMonkeyOnline/viewxml.php
@@ -2,6 +2,25 @@
 include_once('ScoreManager.php');
 
 $manager = new ScoreManager();
-echo $manager->generate_score_xml();
+
+//Get user credentials
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+//Failed message will display if no valid credentials have been provided
+$xml = 'FAILED';
+
+//Login as teacher first, if it succeeds display everything
+if($manager->login_as_teacher($username, $password)) {
+	$xml = $manager->generate_score_xml();
+//Login as any user, only display the user's scores
+} else if($manager->login($username, $password)) {
+	$xml = $manager->generate_user_score_xml($username);
+}
+
+//TODO - Delete the line below once iPhone part is finished!
+$xml = $manager->generate_score_xml();
+
+echo $xml;
 
 ?>
